ce25371ba90927538c3d2a4f33a28948254e36cd
purple <b10705022@ntu.edu.tw>
2023-12-22 20:58:26 +0800

    auth

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,23 @@
+import { redirect } from "next/navigation";
+
+import useAuth from "@/hooks/useAuth";
+import { publicEnv } from "@/lib/env/public";
+
+import AuthForm from "./_components/AuthForm";
+
+export default async function AuthPage() {
+  const { auth } = useAuth();
+
+  // If signed in, redirect to /
+  const session = await auth();
+
+  if (session?.user?.id) {
+    redirect(`${publicEnv.NEXT_PUBLIC_BASE_URL}`);
+  }
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <AuthForm />
+    </main>
+  );
+}
