1adc7e7f071d0d080af55b41812f7b7d6edb2faf
purple <b10705022@ntu.edu.tw>
2023-12-23 20:49:55 +0800

    update components layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,108 +6,43 @@ import useAuth from "@/hooks/useAuth";
 
 import NewSongButton from "@/components/uploadSong";
 import SignInButton from "@/components/SignInButton";
-// import SignOutButton from "@/components/SignOutButton";
-import Navbar from "@/components/Navbar";
+import SignOutButton from "@/components/SignOutButton";
 
-type HomePageProps = {
-  searchParams: {
-    username?: string;
-  };
-};
-
-export default async function Home({
-  searchParams: { username },
-}: HomePageProps) {
+export default async function Home() {
   const { auth } = useAuth();
   const session = await auth();
-  // if (username) {
-  //   await db
-  //     .insert(usersTable)
-  //     .values({
-  //       displayName: username,
-  //       handle,
-  //     })
-  //     .onConflictDoUpdate({
-  //       target: usersTable.handle,
-  //       set: {
-  //         displayName: username,
-  //       },
-  //     })
-  //     .execute();
-  // }
-  // const likesSubquery = db.$with("likes_count").as(
-  //   db
-  //     .select({
-  //       tweetId: likesTable.tweetId,
-  //       likes: sql<number | null>`count(*)`.mapWith(Number).as("likes"),
-  //     })
-  //     .from(likesTable)
-  //     .groupBy(likesTable.tweetId),
-  // );
-
-  // const likedSubquery = db.$with("liked").as(
-  //   db
-  //     .select({
-  //       tweetId: likesTable.tweetId,
-  //       liked: sql<number>`1`.mapWith(Boolean).as("liked"),
-  //     })
-  //     .from(likesTable)
-  //     .where(eq(likesTable.userHandle, handle ?? "")),
-  // );
-
-  // const tweets = await db
-  //   .with(likesSubquery, likedSubquery)
-  //   .select({
-  //     id: tweetsTable.id,
-  //     content: tweetsTable.content,
-  //     username: usersTable.displayName,
-  //     handle: usersTable.handle,
-  //     likes: likesSubquery.likes,
-  //     createdAt: tweetsTable.createdAt,
-  //     liked: likedSubquery.liked,
-  //   })
-  //   .from(tweetsTable)
-  //   .where(and(isNull(tweetsTable.replyToTweetId), (keyWord)? like(tweetsTable.content, `%${keyWord}%`) : notIlike(tweetsTable.content, `%${keyWord}%`)))
-  //   .orderBy(desc(tweetsTable.createdAt))
-  //   .innerJoin(usersTable, eq(tweetsTable.userHandle, usersTable.handle))
-  //   .leftJoin(likesSubquery, eq(tweetsTable.id, likesSubquery.tweetId))
-  //   .leftJoin(likedSubquery, eq(tweetsTable.id, likedSubquery.tweetId))
-  //   .execute();
 
   return (
     <>
-      <div className="flex h-screen w-full flex-col overflow-scroll pt-2">
-        <h1 className="mb-2 bg-black px-4 text-xl font-bold text-center">Music!</h1>
-          <NewSongButton/>
-          {!session?.user?.id? (
-            <SignInButton/>
-          ) : (
-            // 等 sign in 沒問題可以再把 <NewSongButton/> 移進來
-            <Navbar/>
-          )}
+      <div className="flex h-screen w-full flex-col overflow-auto pt-2">
 
-        {/* <div className="flex flex-col w-full content-center items-center px-4 pt-3">
-          <ProfileButton />
+        {/* Header bar goes here */}
+        <div className="flex flex-row items-center justify-between px-4 mb-4">
+          <h1 className="bg-black px-4 text-xl font-bold text-center">Music!</h1>
+          <div className="flex items-center gap-4">
+            {!session?.user?.id ? (
+              <SignInButton />
+            ) : (
+              <>
+                <div className="flex flex-row items-center gap-2">
+                  <NewSongButton />
+                  {/* Move to the profile page later */}
+                  <SignOutButton />
+                </div>
+                <div className="flex items-center gap-2">
+                  {/* Profile page goes here (add a link) */}
+                  <div className="flex h-8 w-8 items-center justify-center rounded-full bg-blue-400"></div>
+                  <span className="text-md font-semibold">
+                    {session?.user?.name}
+                  </span>
+                </div>
+              </>
+            )}
+          </div>
         </div>
-        <div className="w-full flex flex-row px-4 pt-3">
-          <SearchBar />
-          <NewActButton />
-        </div>
-        {tweets.map((tweet) => (
-          <Tweet
-            key={tweet.id}
-            id={tweet.id}
-            username={username}
-            handle={handle}
-            authorName={tweet.username}
-            content={tweet.content}
-            likes={tweet.likes}
-            liked={tweet.liked}
-            createdAt={tweet.createdAt!}
-          />
-        ))} */}
+        
+        {/* Main page goes here */}
       </div>
-      {/* <NameDialog /> */}
     </>
-  );
+  );  
 }
--- src/components/uploadSong.tsx
+++ src/components/uploadSong.tsx
@@ -1,17 +1,18 @@
 "use client";
 import { useState } from 'react';
 import TweetDialog from '@/components/SongDialog'
+import { Button } from "@/components/ui/button";
+
 export default function NewActButton() {
   const [actDia, setActDia] = useState(false);
 
   return (
   <>
-    <button
-      className="flex items-center gap-2 p-3 text-start transition-colors duration-300 border border-gray-800 rounded-none border-solid hover:bg-gray-200"
+    <Button variant={"outline"}
       onClick={() => setActDia(true)}
     >
-      <p className="text-sm font-bold">Publish!</p>
-    </button>
+      Publish!
+    </Button>
     <TweetDialog
      open = {actDia}
      onClose={() => setActDia(false)}
