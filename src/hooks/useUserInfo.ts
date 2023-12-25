39c64d57f71c56c51b8b87150b8da011d49e45c8
Mimi <mimi911123@gmail.com>
2023-12-25 15:12:00 +0800

    user api

--- src/hooks/useUserInfo.ts
+++ src/hooks/useUserInfo.ts
@@ -1,11 +1,32 @@
-import { useMemo } from "react";
+import { useMemo, useState } from "react";
 
-import { useSearchParams } from "next/navigation";
+import { useSearchParams, useRouter } from "next/navigation";
 
 // this is a helper function to get user info in client components
 export default function useUserInfo() {
   const searchParams = useSearchParams();
   const username = useMemo(() => searchParams.get("username"), [searchParams]);
+  const [loading, setLoading] = useState(false);
+  const router = useRouter();
+
+  const getUser = async () => {
+    setLoading(true);
+
+    const res = await fetch("/api/user", {
+      method: "GET",
+    });
+
+    if (!res.ok) {
+      const body = await res.json();
+      throw new Error(body.error);
+    }
+    const data = await res.json();
+    
+    router.refresh();
+    setLoading(false);
+    return data;
+  }
+
   return {
     username,
   };
